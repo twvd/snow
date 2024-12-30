a1a3245d44e4a2c15447e22d62ad6403cd84c7bd
Thomas <thomas@thomasw.dev>
2024-12-30 21:08:45 +0100

    egui: Implement mouse control

--- frontend_egui/src/app.rs
+++ frontend_egui/src/app.rs
@@ -1,12 +1,14 @@
-use eframe::egui;
-use snow_core::mac::MacModel;
-
 use crate::emulator::EmulatorState;
 use crate::widgets::framebuffer::FramebufferWidget;
+use eframe::egui;
+use eframe::egui::{CursorIcon, PointerButton};
+use snow_core::mac::video::{SCREEN_HEIGHT, SCREEN_WIDTH};
+use snow_core::mac::MacModel;
 
 pub struct SnowGui {
     emu: EmulatorState,
     framebuffer: FramebufferWidget,
+    hide_cursor: bool,
 }
 
 impl SnowGui {
@@ -14,6 +16,7 @@ impl SnowGui {
         Self {
             emu: Default::default(),
             framebuffer: FramebufferWidget::new(cc),
+            hide_cursor: false,
         }
     }
 }
@@ -32,7 +35,49 @@ impl eframe::App for SnowGui {
             self.framebuffer.draw(ui);
         });
 
+        if self.hide_cursor {
+            ctx.set_cursor_icon(CursorIcon::None);
+        }
+
         // Re-render as soon as possible to keep the display updating
         ctx.request_repaint();
+    }
+
+    fn raw_input_hook(&mut self, ctx: &egui::Context, raw_input: &mut egui::RawInput) {
+        if ctx.wants_keyboard_input() {
+            return;
+        }
+
+        for event in &raw_input.events {
+            match event {
+                egui::Event::PointerButton {
+                    button: PointerButton::Primary,
+                    pressed,
+                    ..
+                } => {
+                    self.emu.update_mouse_button(*pressed);
+                }
+                egui::Event::MouseMoved(_) => {
+                    if let Some(mouse_pos) = ctx.pointer_hover_pos() {
+                        let fbrect = self.framebuffer.rect();
+                        let scale = egui::Vec2::from([
+                            SCREEN_WIDTH as f32 / fbrect.width(),
+                            SCREEN_HEIGHT as f32 / fbrect.height(),
+                        ]);
+                        let x = (mouse_pos.x - fbrect.left_top().x) * scale.x;
+                        let y = (mouse_pos.y - fbrect.left_top().y) * scale.y;
+                        if x < 0.0 || y < 0.0 || x > SCREEN_WIDTH as f32 || y > SCREEN_HEIGHT as f32
+                        {
+                            continue;
+                        }
+
+                        // Cursor is within framebuffer view area
+                        self.hide_cursor = true;
+                        self.emu.update_mouse(egui::Pos2::from([x, y]));
+                    }
+                }
+                _ => (),
+            }
+        }
     }
 }
--- frontend_egui/src/emulator.rs
+++ frontend_egui/src/emulator.rs
@@ -2,6 +2,7 @@
 
 use anyhow::Result;
 use crossbeam_channel::Receiver;
+use eframe::egui;
 use snow_core::emulator::comm::EmulatorCommandSender;
 use snow_core::emulator::comm::{EmulatorCommand, EmulatorSpeed};
 use snow_core::emulator::Emulator;
@@ -23,7 +24,7 @@ impl EmulatorState {
         let (mut emulator, frame_recv) = Emulator::new(rom, model)?;
         let cmd = emulator.create_cmd_sender();
         // TODO audio
-        cmd.send(EmulatorCommand::SetSpeed(EmulatorSpeed::Video))?;
+        cmd.send(EmulatorCommand::SetSpeed(EmulatorSpeed::Uncapped))?;
         cmd.send(EmulatorCommand::Run)?;
 
         // Spin up emulator thread
@@ -39,5 +40,28 @@ impl EmulatorState {
         self.emuthread = Some(emuthread);
 
         Ok(frame_recv)
+    }
+
+    pub fn update_mouse(&mut self, p: egui::Pos2) {
+        if let Some(ref sender) = self.cmdsender {
+            sender
+                .send(EmulatorCommand::MouseUpdateAbsolute {
+                    x: p.x as u16,
+                    y: p.y as u16,
+                })
+                .unwrap();
+        }
+    }
+
+    pub fn update_mouse_button(&mut self, state: bool) {
+        if let Some(ref sender) = self.cmdsender {
+            sender
+                .send(EmulatorCommand::MouseUpdateRelative {
+                    relx: 0,
+                    rely: 0,
+                    btn: Some(state),
+                })
+                .unwrap();
+        }
     }
 }
