cee9b10ee100778a2ad78104ffb18d1c120e0ec9
Thomas <thomas@thomasw.dev>
2024-12-30 15:32:01 +0100

    Refactor into FramebufferWidget

--- frontend_egui/src/main.rs
+++ frontend_egui/src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-mod app;
+pub mod app;
+pub mod widgets;
 
 use crate::app::SnowGui;
 use clap::Parser;
---
+++ frontend_egui/src/widgets/framebuffer.rs
@@ -0,0 +1,68 @@
+//! Widget that receives frames from the emulator and draws them to a
+//! GPU texture-backed image widget.
+
+use std::sync::atomic::Ordering;
+
+use crossbeam_channel::Receiver;
+use eframe::egui;
+use eframe::egui::Vec2;
+use snow_core::mac::video::{SCREEN_HEIGHT, SCREEN_WIDTH};
+use snow_core::renderer::DisplayBuffer;
+
+pub struct FramebufferWidget {
+    frame_recv: Option<Receiver<DisplayBuffer>>,
+    viewport_texture: egui::TextureHandle,
+}
+
+impl FramebufferWidget {
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self {
+            frame_recv: None,
+            viewport_texture: cc.egui_ctx.load_texture(
+                "viewport",
+                egui::ColorImage::example(),
+                egui::TextureOptions::NEAREST,
+            ),
+        }
+    }
+
+    #[inline(always)]
+    fn convert_framebuffer(framebuffer: DisplayBuffer) -> Vec<egui::Color32> {
+        // TODO optimize this
+        let mut out = Vec::with_capacity(SCREEN_WIDTH * SCREEN_HEIGHT);
+
+        for c in framebuffer.chunks(4) {
+            out.push(egui::Color32::from_rgb(
+                c[0].load(Ordering::Relaxed),
+                c[1].load(Ordering::Relaxed),
+                c[2].load(Ordering::Relaxed),
+            ));
+        }
+
+        out
+    }
+
+    pub fn connect_receiver(&mut self, recv: Receiver<DisplayBuffer>) {
+        self.frame_recv = Some(recv);
+    }
+
+    pub fn draw(&mut self, ui: &mut egui::Ui) {
+        if let Some(ref frame_recv) = self.frame_recv {
+            if !frame_recv.is_empty() {
+                let frame = frame_recv.recv().unwrap();
+
+                self.viewport_texture.set(
+                    egui::ColorImage {
+                        size: [SCREEN_WIDTH, SCREEN_HEIGHT],
+                        pixels: Self::convert_framebuffer(frame),
+                    },
+                    egui::TextureOptions::NEAREST,
+                );
+            }
+        }
+
+        let size = self.viewport_texture.size_vec2();
+        let sized_texture = egui::load::SizedTexture::new(&mut self.viewport_texture, size);
+        ui.add(egui::Image::new(sized_texture).fit_to_fraction(Vec2::new(1.0, 1.0)));
+    }
+}
