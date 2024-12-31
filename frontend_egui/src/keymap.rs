cb8cabae53cbf49a260ee1276705e25f41a24a54
Thomas <thomas@thomasw.dev>
2024-12-31 17:01:26 +0100

    egui: patch into winit for better keyboard input events

--- frontend_egui/src/keymap.rs
+++ frontend_egui/src/keymap.rs
@@ -142,3 +142,148 @@ pub fn map_egui_keycode(kc: egui::Key) -> Option<Scancode> {
         _ => None,
     }
 }
+
+/// Maps a winit keycode to 'Snow universal'
+pub fn map_winit_keycode(kc: egui_winit::winit::keyboard::KeyCode) -> Option<Scancode> {
+    use egui_winit::winit::keyboard::KeyCode;
+
+    match kc {
+        // ,---.   .---------------. ,---------------. ,---------------. ,-----------.             ,---.
+        // |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau|             |Pwr|
+        // | 35|   | 7A| 78| 63| 76| | 60| 61| 62| 64| | 65| 6D| 67| 6F| | 69| 6B| 71|             | 7F|
+        // `---'   `---------------' `---------------' `---------------' `-----------'             `---'
+        KeyCode::Escape => Some(0x35),
+        KeyCode::F1 => Some(0x7A),
+        KeyCode::F2 => Some(0x78),
+        KeyCode::F3 => Some(0x63),
+        KeyCode::F4 => Some(0x76),
+        KeyCode::F5 => Some(0x60),
+        KeyCode::F6 => Some(0x61),
+        KeyCode::F7 => Some(0x62),
+        KeyCode::F8 => Some(0x64),
+        KeyCode::F9 => Some(0x65),
+        KeyCode::F10 => Some(0x6D),
+        KeyCode::F11 => Some(0x67),
+        KeyCode::F12 => Some(0x6F),
+        KeyCode::PrintScreen => Some(0x69),
+        KeyCode::ScrollLock => Some(0x6B),
+        KeyCode::Pause => Some(0x71),
+
+        // ,-----------------------------------------------------------. ,-----------. ,---------------.
+        // |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backspa| |Ins|Hom|PgU| |NmL|  =|  /|  *|
+        // | 32| 12| 13| 14| 15| 17| 16| 1A| 1C| 19| 1D| 1B| 18|   33  | | 72| 73| 74| | 47| 51| 4B| 43|
+        // |-----------------------------------------------------------| |-----------| |---------------|
+        KeyCode::Backquote => Some(0x32),
+        KeyCode::Digit1 => Some(0x12),
+        KeyCode::Digit2 => Some(0x13),
+        KeyCode::Digit3 => Some(0x14),
+        KeyCode::Digit4 => Some(0x15),
+        KeyCode::Digit5 => Some(0x17),
+        KeyCode::Digit6 => Some(0x16),
+        KeyCode::Digit7 => Some(0x1A),
+        KeyCode::Digit8 => Some(0x1C),
+        KeyCode::Digit9 => Some(0x19),
+        KeyCode::Digit0 => Some(0x1D),
+        KeyCode::Minus => Some(0x1B),
+        KeyCode::Equal => Some(0x18),
+        KeyCode::Backspace => Some(0x33),
+
+        KeyCode::Insert => Some(0x72),
+        KeyCode::Home => Some(0x73),
+        KeyCode::PageUp => Some(0x74),
+
+        KeyCode::NumLock => Some(0x47),
+        KeyCode::NumpadEqual => Some(0x51),
+        KeyCode::NumpadDivide => Some(0x4B),
+        KeyCode::NumpadMultiply => Some(0x43),
+
+        // |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  | |Del|End|PgD| |  7|  8|  9|  -|
+        // |  30 | 0C| 0D| 0E| 0F| 11| 10| 20| 22| 1F| 23| 21| 1E|  2A | | 75| 77| 79| | 59| 5B| 5C| 4E|
+        // |-----------------------------------------------------------| `-----------' |---------------|
+        KeyCode::Tab => Some(0x30),
+        KeyCode::KeyQ => Some(0x0C),
+        KeyCode::KeyW => Some(0x0D),
+        KeyCode::KeyE => Some(0x0E),
+        KeyCode::KeyR => Some(0x0F),
+        KeyCode::KeyT => Some(0x11),
+        KeyCode::KeyY => Some(0x10),
+        KeyCode::KeyU => Some(0x20),
+        KeyCode::KeyI => Some(0x22),
+        KeyCode::KeyO => Some(0x1F),
+        KeyCode::KeyP => Some(0x23),
+        KeyCode::BracketLeft => Some(0x21),
+        KeyCode::BracketRight => Some(0x1E),
+        KeyCode::Backslash => Some(0x2A),
+
+        KeyCode::Delete => Some(0x75),
+        KeyCode::End => Some(0x77),
+        KeyCode::PageDown => Some(0x79),
+
+        KeyCode::Numpad7 => Some(0x59),
+        KeyCode::Numpad8 => Some(0x5B),
+        KeyCode::Numpad9 => Some(0x5C),
+
+        // |CapsLo|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|  Return|               |  4|  5|  6|  +|
+        // |  39  | 00| 01| 02| 03| 05| 04| 26| 28| 25| 29| 27|   24   |               | 56| 57| 58| 45|
+        // |-----------------------------------------------------------|     ,---.     |---------------|
+        KeyCode::CapsLock => Some(0x39),
+        KeyCode::KeyA => Some(0x00),
+        KeyCode::KeyS => Some(0x01),
+        KeyCode::KeyD => Some(0x02),
+        KeyCode::KeyF => Some(0x03),
+        KeyCode::KeyG => Some(0x05),
+        KeyCode::KeyH => Some(0x04),
+        KeyCode::KeyJ => Some(0x26),
+        KeyCode::KeyK => Some(0x28),
+        KeyCode::KeyL => Some(0x25),
+        KeyCode::Semicolon => Some(0x29),
+        KeyCode::Quote => Some(0x27),
+        KeyCode::Enter => Some(0x24),
+
+        KeyCode::Numpad4 => Some(0x56),
+        KeyCode::Numpad5 => Some(0x57),
+        KeyCode::Numpad6 => Some(0x58),
+        KeyCode::NumpadAdd => Some(0x45),
+
+        // |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |     |Up |     |  1|  2|  3|   |
+        // |   38   | 06| 07| 08| 09| 0B| 2D| 2E| 2B| 2F| 2C|    7B    |     | 3E|     | 53| 54| 55|   |
+        // |-----------------------------------------------------------| ,-----------. |-----------|Ent|
+        KeyCode::ShiftLeft => Some(0x38),
+        KeyCode::KeyZ => Some(0x06),
+        KeyCode::KeyX => Some(0x07),
+        KeyCode::KeyC => Some(0x08),
+        KeyCode::KeyV => Some(0x09),
+        KeyCode::KeyB => Some(0x0B),
+        KeyCode::KeyN => Some(0x2D),
+        KeyCode::KeyM => Some(0x2E),
+        KeyCode::Comma => Some(0x2B),
+        KeyCode::Period => Some(0x2F),
+        KeyCode::Slash => Some(0x2C),
+        KeyCode::ShiftRight => Some(0x7B),
+        KeyCode::ArrowUp => Some(0x3E),
+
+        KeyCode::Numpad1 => Some(0x53),
+        KeyCode::Numpad2 => Some(0x54),
+        KeyCode::Numpad3 => Some(0x55),
+
+        KeyCode::NumpadEnter => Some(0x4C),
+
+        // |Ctrl |Opt | Cmd |        Space            | Cmd |Opt |Ctrl | |Lef|Dow|Rig| |      0|  .|4C |
+        // |  36 | 3A |  37 |           31            |  37 | 7C |  7D | | 3B| 3D| 3C| |    52 | 41|   |
+        // `-----------------------------------------------------------' `-----------' `---------------'
+        KeyCode::ControlLeft => Some(0x36),
+        KeyCode::AltLeft => Some(0x3A),
+        KeyCode::SuperLeft => Some(0x37),
+        KeyCode::Space => Some(0x31),
+        KeyCode::SuperRight => Some(0x37),
+        KeyCode::AltRight => Some(0x7C),
+        KeyCode::ControlRight => Some(0x7D),
+        KeyCode::ArrowLeft => Some(0x3B),
+        KeyCode::ArrowDown => Some(0x3D),
+        KeyCode::ArrowRight => Some(0x3C),
+
+        KeyCode::Numpad0 => Some(0x52),
+        KeyCode::NumpadDecimal => Some(0x41),
+        _ => None,
+    }
+}
