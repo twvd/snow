9eebc23fa89f5490cc85bc665ff0e20b74810062
Thomas <thomas@thomasw.dev>
2025-03-09 15:06:14 +0100

    Implement bus access breakpoints

--- frontend_egui/src/widgets/breakpoints.rs
+++ frontend_egui/src/widgets/breakpoints.rs
@@ -2,11 +2,14 @@ use crate::emulator::EmulatorState;
 use eframe::egui;
 use eframe::egui::RichText;
 use snow_core::bus::Address;
-use snow_core::cpu_m68k::cpu::Breakpoint;
+use snow_core::cpu_m68k::cpu::{Breakpoint, BusBreakpoint};
 
 #[derive(Default)]
 pub struct BreakpointsWidget {
-    newbp_input: String,
+    exec_input: String,
+    bus_input: String,
+    bus_r: bool,
+    bus_w: bool,
     added_bp: Option<Breakpoint>,
 }
 
@@ -16,21 +19,50 @@ impl BreakpointsWidget {
         let available_height = ui.available_height();
 
         ui.vertical(|ui| {
-            ui.horizontal(|ui| {
-                ui.label("Address (hex): ");
-                ui.text_edit_singleline(&mut self.newbp_input);
-                if ui
-                    .add_enabled(
-                        Address::from_str_radix(&self.newbp_input, 16).is_ok(),
-                        egui::Button::new("Add breakpoint"),
-                    )
-                    .clicked()
-                {
-                    self.added_bp = Some(Breakpoint::Execution(
-                        Address::from_str_radix(&self.newbp_input, 16).unwrap(),
-                    ));
-                    self.newbp_input.clear();
-                }
+            ui.collapsing("Add execution breakpoint", |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Address (hex): ");
+                    ui.text_edit_singleline(&mut self.exec_input);
+                    if ui
+                        .add_enabled(
+                            Address::from_str_radix(&self.exec_input, 16).is_ok_and(|a| a & 1 == 0),
+                            egui::Button::new("Add breakpoint"),
+                        )
+                        .clicked()
+                    {
+                        self.added_bp = Some(Breakpoint::Execution(
+                            Address::from_str_radix(&self.exec_input, 16).unwrap(),
+                        ));
+                        self.exec_input.clear();
+                    }
+                });
+            });
+            ui.collapsing("Add bus access breakpoint", |ui| {
+                ui.horizontal(|ui| {
+                    ui.label("Address (hex): ");
+                    ui.text_edit_singleline(&mut self.bus_input);
+                    ui.checkbox(&mut self.bus_r, "R");
+                    ui.checkbox(&mut self.bus_w, "W");
+                    if ui
+                        .add_enabled(
+                            Address::from_str_radix(&self.bus_input, 16).is_ok()
+                                && (self.bus_r || self.bus_w),
+                            egui::Button::new("Add breakpoint"),
+                        )
+                        .clicked()
+                    {
+                        self.added_bp = Some(Breakpoint::Bus(
+                            match (self.bus_r, self.bus_w) {
+                                (true, false) => BusBreakpoint::Read,
+                                (false, true) => BusBreakpoint::Write,
+                                (true, true) => BusBreakpoint::ReadWrite,
+                                _ => unreachable!(),
+                            },
+                            Address::from_str_radix(&self.bus_input, 16).unwrap(),
+                        ));
+                        self.exec_input.clear();
+                    }
+                });
             });
             ui.separator();
 
@@ -38,7 +70,7 @@ impl BreakpointsWidget {
                 .max_scroll_height(available_height)
                 .auto_shrink(false)
                 .column(Column::exact(20.0))
-                .column(Column::exact(100.0))
+                .column(Column::remainder())
                 .striped(true)
                 .body(|mut body| {
                     for &bp in state.get_breakpoints() {
@@ -50,8 +82,18 @@ impl BreakpointsWidget {
                             });
                             row.col(|ui| {
                                 ui.label(RichText::from(match bp {
-                                    Breakpoint::Execution(addr) => format!("{:06X}", addr),
-                                    Breakpoint::Bus(_, _) => todo!(),
+                                    Breakpoint::Execution(addr) => {
+                                        format!("Execution: ${:06X}", addr)
+                                    }
+                                    Breakpoint::Bus(BusBreakpoint::Read, addr) => {
+                                        format!("Bus access (R): ${:06X}", addr)
+                                    }
+                                    Breakpoint::Bus(BusBreakpoint::Write, addr) => {
+                                        format!("Bus access (W): ${:06X}", addr)
+                                    }
+                                    Breakpoint::Bus(BusBreakpoint::ReadWrite, addr) => {
+                                        format!("Bus access (R/W): ${:06X}", addr)
+                                    }
                                     Breakpoint::InterruptLevel(_) => todo!(),
                                     Breakpoint::InterruptVector(_) => todo!(),
                                     Breakpoint::LineA(_) => todo!(),
