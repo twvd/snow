18eb8154bae40d76da72d6fb6e998dbb382471eb
Thomas <thomas@thomasw.dev>
2025-06-08 11:56:36 +0200

    Implement FPU registers

--- core/src/cpu_m68k/fpu/mod.rs
+++ core/src/cpu_m68k/fpu/mod.rs
@@ -1,1 +1,13 @@
+use arpfloat::{RoundingMode, Semantics};
+
 pub mod ops_generic;
+pub mod regs;
+
+/// 6888x/68040 single precision float semantics
+pub const SEMANTICS_SINGLE: Semantics = Semantics::new(8, 24, RoundingMode::NearestTiesToEven);
+
+/// 6888x/68040 double precision float semantics
+pub const SEMANTICS_DOUBLE: Semantics = Semantics::new(11, 53, RoundingMode::NearestTiesToEven);
+
+/// 6888x/68040 extended precision float semantics
+pub const SEMANTICS_EXTENDED: Semantics = Semantics::new(15, 64, RoundingMode::NearestTiesToEven);
