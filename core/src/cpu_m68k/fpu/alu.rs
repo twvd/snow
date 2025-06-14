40c33ef0baa0a5f8f34429bf5aa8fab6c7fdd614
Thomas <thomas@thomasw.dev>
2025-06-14 12:18:14 +0200

    FPU: implement FABS, FINT, FCMP, FMUL

--- core/src/cpu_m68k/fpu/alu.rs
+++ core/src/cpu_m68k/fpu/alu.rs
@@ -11,13 +11,30 @@ impl<TBus, const ADDRESS_MASK: Address, const CPU_TYPE: CpuM68kType>
 where
     TBus: Bus<Address, u8> + IrqSource,
 {
-    pub(in crate::cpu_m68k) fn fpu_alu_op(&mut self, opmode: u8, source: &Float) -> Result<Float> {
+    pub(in crate::cpu_m68k) fn fpu_alu_op(
+        &mut self,
+        opmode: u8,
+        source: &Float,
+        dest: &Float,
+    ) -> Result<Float> {
         let result = match opmode {
-            0b000000 => {
-                // FMOVE
-                source.clone()
+            // FMOVE
+            0b0000000 => source.clone(),
+            // FABS
+            0b0011000 => source.abs(),
+            // FMUL
+            0b0100011 => source * dest,
+            // FINT
+            // TODO rounding mode
+            0b0000001 => source.round(),
+            // FCMP
+            0b0111000 => {
+                let result = dest - source;
+                self.fpu_condition_codes(&result);
+                // TODO flags
+                return Ok(dest.clone());
             }
-            _ => bail!("Unimplemented FPU ALU op {:06b}", opmode),
+            _ => bail!("Unimplemented FPU ALU op {:07b}", opmode),
         };
 
         // Flags
@@ -30,11 +47,15 @@ where
         self.regs.fpu.fpsr.exs_mut().set_inex1(false); // * P
 
         // Condition codes (3.6.2)
+        self.fpu_condition_codes(&result);
+
+        Ok(result)
+    }
+
+    fn fpu_condition_codes(&mut self, result: &Float) {
         self.regs.fpu.fpsr.set_fpcc_nan(result.is_nan());
         self.regs.fpu.fpsr.set_fpcc_i(result.is_inf());
         self.regs.fpu.fpsr.set_fpcc_n(result.is_negative());
         self.regs.fpu.fpsr.set_fpcc_z(result.is_zero());
-
-        Ok(result)
     }
 }
