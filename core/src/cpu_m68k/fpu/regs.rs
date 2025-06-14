0950230e1ccafb3cbd3d8df5ed94418c3fe5925f
Thomas <thomas@thomasw.dev>
2025-06-14 19:00:10 +0200

    Show FPU register changes in instruction history

--- core/src/cpu_m68k/fpu/regs.rs
+++ core/src/cpu_m68k/fpu/regs.rs
@@ -123,6 +123,36 @@ pub struct FpuRegisterFile {
     pub fpiar: Address,
 }
 
+impl FpuRegisterFile {
+    /// Creates a string with differences between this FpuRegisterFile and another
+    pub fn diff_str(&self, other: &Self) -> String {
+        let diff = |name, s, o| {
+            if s != o {
+                format!("{}: {:08X} -> {:08X} ", name, s, o)
+            } else {
+                String::new()
+            }
+        };
+        let diff_fp = |name, s: &Float, o: &Float| {
+            if s.is_inf() != o.is_inf()
+                || s.is_nan() != o.is_nan()
+                || (!s.is_nan() && !s.is_inf() && s.as_f64() != o.as_f64())
+            {
+                format!("{}: {:0.4} -> {:0.4} ", name, s.as_f64(), o.as_f64())
+            } else {
+                String::new()
+            }
+        };
+        let mut out = String::new();
+        for i in 0..8 {
+            out.push_str(diff_fp(format!("FP{}", i), &self.fp[i], &other.fp[i]).as_str());
+        }
+        out.push_str(diff("FPCR".to_string(), self.fpcr.0, other.fpcr.0).as_str());
+        out.push_str(diff("FPSR".to_string(), self.fpsr.0, other.fpsr.0).as_str());
+        out
+    }
+}
+
 impl Default for FpuRegisterFile {
     fn default() -> Self {
         Self {
