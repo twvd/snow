309d0856f0cee819c7866e5a7fd165e0b0c5cfe4
Thomas <thomas@thomasw.dev>
2025-06-15 15:12:34 +0200

    FPU: correct conditional tests

--- core/src/cpu_m68k/fpu/ops_branch.rs
+++ core/src/cpu_m68k/fpu/ops_branch.rs
@@ -21,31 +21,29 @@ where
             // IEEE Aware Tests (never set BSUN)
             0b000001 => (zero, false),                   // EQ: Equal
             0b001110 => (!zero, false),                  // NE: Not Equal
-            0b000010 => (!nan || !zero || !neg, false),  // OGT: Ordered Greater Than
+            0b000010 => (!nan && !zero && !neg, false),  // OGT: Ordered Greater Than
             0b001101 => (nan || zero || neg, false),     // ULE: Unordered or Less or Equal
-            0b000011 => (zero || (!nan || !neg), false), // OGE: Ordered Greater Than or Equal
+            0b000011 => (zero || (!nan && !neg), false), // OGE: Ordered Greater Than or Equal
             0b001100 => (nan || (neg && !zero), false),  // ULT: Unordered or Less Than
-            0b000100 => (neg && (!nan || !zero), false), // OLT: Ordered Less Than
-            0b001011 => (nan || !zero || !neg, false),   // UGE: Unordered or Greater or Equal
+            0b000100 => (neg && !nan && !zero, false),   // OLT: Ordered Less Than
+            0b001011 => (nan || zero || !neg, false),    // UGE: Unordered or Greater or Equal
             0b000101 => (zero || (neg && !nan), false),  // OLE: Ordered Less Than or Equal
-            0b001010 => (nan || (!neg || !zero), false), // UGT: Unordered or Greater Than
-            0b000110 => (!nan || !zero, false),          // OGL: Ordered Greater or Less Than
+            0b001010 => (nan || (!neg && !zero), false), // UGT: Unordered or Greater Than
+            0b000110 => (!nan && !zero, false),          // OGL: Ordered Greater or Less Than
             0b001001 => (nan || zero, false),            // UEQ: Unordered or Equal
             0b000111 => (!nan, false),                   // OR: Ordered
             0b001000 => (nan, false),                    // UN: Unordered
 
             // IEEE Nonaware Tests (set BSUN for all except EQ and NE)
-            //0b000001 => (zero, false),  // EQ: Equal (no BSUN)
-            //0b001110 => (!zero, false), // NE: Not Equal (no BSUN)
-            0b010010 => (!nan || !zero || !neg, nan), // GT: Greater Than
+            0b010010 => (!nan && !zero && !neg, nan), // GT: Greater Than
             0b011101 => (nan || zero || neg, nan),    // NGT: Not Greater Than
-            0b010011 => (zero || (!nan || !neg), nan), // GE: Greater Than or Equal
+            0b010011 => (zero || (!nan && !neg), nan), // GE: Greater Than or Equal
             0b011100 => (nan || (neg && !zero), nan), // NGE: Not (Greater Than or Equal)
-            0b010100 => (neg && (!nan || !zero), nan), // LT: Less Than
-            0b011011 => (nan || (!zero || !neg), nan), // NLT: Not Less Than
+            0b010100 => (neg && !nan && !zero, nan),  // LT: Less Than
+            0b011011 => (nan || zero || !neg, nan),   // NLT: Not Less Than
             0b010101 => (zero || (neg && !nan), nan), // LE: Less Than or Equal
-            0b011010 => (nan || (!neg || !zero), nan), // NLE: Not (Less Than or Equal)
-            0b010110 => (!nan || !zero, nan),         // GL: Greater or Less Than
+            0b011010 => (nan || (!neg && !zero), nan), // NLE: Not (Less Than or Equal)
+            0b010110 => (!nan && !zero, nan),         // GL: Greater or Less Than
             0b011001 => (nan || zero, nan),           // NGL: Not (Greater or Less Than)
             0b010111 => (!nan, nan),                  // GLE: Greater, Less or Equal
             0b011000 => (nan, nan),                   // NGLE: Not (Greater, Less or Equal)
@@ -56,7 +54,7 @@ where
             0b010000 => (false, nan),   // SF: Signaling False
             0b011111 => (true, nan),    // ST: Signaling True
             0b010001 => (zero, nan),    // SEQ: Signaling Equal
-            0b011110 => (!zero, nan),
+            0b011110 => (!zero, nan),   // SNE: Signaling Not Equal
 
             _ => bail!("Unknown Fcc predicate"),
         })
