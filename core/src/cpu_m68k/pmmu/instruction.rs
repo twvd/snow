24c3ad25e1eb25f3f9e6ae7f5e99a83b83ac5741
Thomas <thomas@thomasw.dev>
2025-07-24 23:23:30 +0200

    PMMU: PMOVE Preg,ea / ea,Preg

--- core/src/cpu_m68k/pmmu/instruction.rs
+++ core/src/cpu_m68k/pmmu/instruction.rs
@@ -1,3 +1,5 @@
+//! M68851 PMMU - Instruction decoding
+
 use proc_bitfield::bitfield;
 
 use crate::types::Word;
@@ -6,7 +8,11 @@ bitfield! {
     /// PMOVE format 1
     #[derive(Clone, Copy, PartialEq, Eq)]
     pub struct Pmove1Extword(pub Word): Debug, FromStorage, IntoStorage, DerefStorage {
+        /// True: register to EA
+        /// False: EA to register
         pub write: bool @ 9,
+
+        /// PMMU register select
         pub preg: usize @ 10..=12,
     }
 }
