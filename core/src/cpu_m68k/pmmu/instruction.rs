c0440a25140496766346e0a9d85d0f7c95438a88
Thomas <thomas@thomasw.dev>
2025-08-02 20:40:21 +0200

    PMMU: implement PTEST, PMOVE3

--- core/src/cpu_m68k/pmmu/instruction.rs
+++ core/src/cpu_m68k/pmmu/instruction.rs
@@ -16,3 +16,28 @@ bitfield! {
         pub preg: usize @ 10..=12,
     }
 }
+
+bitfield! {
+    /// PMOVE format 3
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    pub struct Pmove3Extword(pub Word): Debug, FromStorage, IntoStorage, DerefStorage {
+        /// True: register to EA
+        /// False: EA to register
+        pub write: bool @ 9,
+
+        /// PMMU register select
+        pub preg: usize @ 10..=12,
+    }
+}
+
+bitfield! {
+    /// PTEST
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    pub struct PtestExtword(pub Word): Debug, FromStorage, IntoStorage, DerefStorage {
+        pub fc: u8 @ 0..=4,
+        pub an: usize @ 5..=7,
+        pub a_set: bool @ 8,
+        pub read: bool @ 9,
+        pub level: u8 @ 10..=12,
+    }
+}
