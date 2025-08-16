8d88b984545d4396c4be3f20b45afbca82c36b44
Thomas <thomas@thomasw.dev>
2025-08-16 14:27:10 +0200

    Implement hardware emulation of ADB mouse

--- core/src/mac/adb/mouse.rs
+++ core/src/mac/adb/mouse.rs
@@ -1,24 +1,50 @@
-use crate::types::{ClickEventReceiver, ClickEventSender};
+use crate::{
+    types::{MouseEventReceiver, MouseEventSender},
+    util::take_from_accumulator,
+};
 
 use super::{AdbDevice, AdbDeviceResponse, AdbReg3};
 
 use log::*;
+use proc_bitfield::bitfield;
+
+bitfield! {
+    /// Talk Register 0
+    #[derive(Clone, Copy, PartialEq, Eq, Default)]
+    pub struct AdbMouseReg0(pub u16): Debug, FromStorage, IntoStorage, DerefStorage {
+        /// Buffered relative X motion
+        pub x: i8 @ 0..=6,
+        /// Buffered relative Y motion
+        pub y: i8 @ 8..=14,
+        /// Primary button (inverted)
+        pub btn: bool @ 15,
+    }
+}
+
+const MAX_REL_MOVE: i32 = 31;
+const MOUSE_FACTOR: i32 = 1;
 
 /// Apple Desktop Bus-connected mouse
 pub struct AdbMouse {
     address: u8,
-    button_recv: ClickEventReceiver,
+    event_recv: MouseEventReceiver,
+    button: bool,
+    rel_move_x: i32,
+    rel_move_y: i32,
 }
 
 impl AdbMouse {
     pub const INITIAL_ADDRESS: u8 = 3;
 
-    pub fn new() -> (Self, ClickEventSender) {
+    pub fn new() -> (Self, MouseEventSender) {
         let (s, button_recv) = crossbeam_channel::unbounded();
         (
             Self {
-                button_recv,
+                event_recv: button_recv,
                 address: Self::INITIAL_ADDRESS,
+                button: false,
+                rel_move_x: 0,
+                rel_move_y: 0,
             },
             s,
         )
@@ -36,20 +62,40 @@ impl AdbDevice for AdbMouse {
     }
 
     fn flush(&mut self) {
-        while !self.button_recv.is_empty() {
-            let _ = self.button_recv.recv();
+        while !self.event_recv.is_empty() {
+            let _ = self.event_recv.recv();
         }
+        self.rel_move_x = 0;
+        self.rel_move_y = 0;
+        self.button = false;
     }
 
     fn talk(&mut self, reg: u8) -> AdbDeviceResponse {
         match reg {
             0 => {
-                if !self.button_recv.is_empty() {
-                    let mut button = false;
-                    while !self.button_recv.is_empty() {
-                        button = self.button_recv.recv().unwrap();
+                if self.get_srq() {
+                    while !self.event_recv.is_empty() {
+                        let event = self.event_recv.recv().unwrap();
+                        if let Some(btn) = event.button {
+                            self.button = btn;
+                        }
+                        if let Some(rel_move) = event.rel_movement {
+                            self.rel_move_x =
+                                self.rel_move_x.saturating_add(rel_move.0 * MOUSE_FACTOR);
+                            self.rel_move_y =
+                                self.rel_move_y.saturating_add(rel_move.1 * MOUSE_FACTOR);
+                        }
                     }
-                    AdbDeviceResponse::from_iter([if button { 0x00_u8 } else { 0x80_u8 }, 0x00_u8])
+                    let motion_x = take_from_accumulator(&mut self.rel_move_x, MAX_REL_MOVE) as i8;
+                    let motion_y = take_from_accumulator(&mut self.rel_move_y, MAX_REL_MOVE) as i8;
+
+                    AdbDeviceResponse::from_iter(
+                        AdbMouseReg0::default()
+                            .with_btn(!self.button)
+                            .with_x(motion_x)
+                            .with_y(motion_y)
+                            .to_be_bytes(),
+                    )
                 } else {
                     AdbDeviceResponse::default()
                 }
@@ -94,6 +140,6 @@ impl AdbDevice for AdbMouse {
     }
 
     fn get_srq(&self) -> bool {
-        !self.button_recv.is_empty()
+        !self.event_recv.is_empty() || self.rel_move_x != 0 || self.rel_move_y != 0
     }
 }
--- core/src/util/mod.rs
+++ core/src/util/mod.rs
@@ -1,1 +1,22 @@
+use std::ops::{Mul, SubAssign};
+
+use num::{PrimInt, Signed};
+
 pub mod lossyinto;
+
+pub fn take_from_accumulator<T: PrimInt + Signed + Mul<Output = T> + SubAssign>(
+    accumulator: &mut T,
+    max_amount: T,
+) -> T {
+    if *accumulator == T::zero() {
+        return T::zero();
+    }
+
+    let sign = accumulator.signum();
+    let available = accumulator.abs();
+    let take_amount = max_amount.min(available);
+    let actual_taken = sign * take_amount;
+
+    *accumulator -= actual_taken;
+    actual_taken
+}
