twvd/snow

commit 1ca9f3ef79936d2f04b02f2f139801c93bcbaa41

Fix Interrupt/Trace exception order
Moves the handling of Trace and Interrupt exceptions to after the current instruction has finished execution to match the 68000.
Trace exceptions are also handled before interrupts if both occur at the same time.

core/src/cpu_m68k/cpu.rs
@@ -401,31 +401,13 @@ where
         self.step_exception = false;
         self.step_over_addr = None;
 
-        // Check pending interrupts
-        if let Some(level) = self.bus.get_irq() {
-            if level == 7 || self.regs.sr.int_prio_mask() < level {
-                if self
-                    .breakpoints
-                    .contains(&Breakpoint::InterruptLevel(level))
-                {
-                    info!(
-                        "Breakpoint hit (interrupt level): {}, PC: ${:08X}",
-                        level, self.regs.pc
-                    );
-                    self.breakpoint_hit.set();
-                }
-
-                self.raise_irq(
-                    level,
-                    VECTOR_AUTOVECTOR_OFFSET + (Address::from(level - 1) * 4),
-                )?;
-            }
-        }
-
-        // Check pending trace
-        if self.regs.sr.trace() && !self.trace_mask {
-            self.raise_exception(ExceptionGroup::Group1, VECTOR_TRACE, None)?;
-        }
+        // Flag exceptions before executing the instruction to act on them later
+        let trace_exception = self.regs.sr.trace() && !self.trace_mask;
+        let irq_exception = match self.bus.get_irq() {
+            Some(7) => true,
+            Some(level) => self.regs.sr.int_prio_mask() < level,
+            _ => false,
+        };
 
         // Start of instruction execution
         if self.history_enabled {
@@ -511,6 +493,31 @@ where
         };
 
         self.prefetch_refill()?;
+
+        // Check pending trace
+        if trace_exception {
+            self.raise_exception(ExceptionGroup::Group1, VECTOR_TRACE, None)?;
+        }
+
+        // Check pending interrupts
+        if irq_exception {
+            let level = self.bus.get_irq().unwrap();
+            if self
+                .breakpoints
+                .contains(&Breakpoint::InterruptLevel(level))
+            {
+                info!(
+                    "Breakpoint hit (interrupt level): {}, PC: ${:08X}",
+                    level, self.regs.pc
+                );
+                self.breakpoint_hit.set();
+            }
+
+            self.raise_irq(
+                level,
+                VECTOR_AUTOVECTOR_OFFSET + (Address::from(level - 1) * 4),
+            )?;
+        }
 
         // Test breakpoint on next PC location
         if self

commit b007216596053726e663e9c071e84926d54e5baf

M68k: fix interrupt masking on instructions that mask interrupts
Fixes: 1ca9f3e
Fixes: #206

core/src/cpu_m68k/cpu.rs
@@ -551,11 +551,7 @@ where
 
         // Flag exceptions before executing the instruction to act on them later
         let trace_exception = self.regs.sr.trace() && !self.trace_mask;
-        let irq_exception = match self.bus.get_irq() {
-            Some(7) => 7,
-            Some(level) if level > self.regs.sr.int_prio_mask() => level,
-            _ => 0,
-        };
+        let irq_exception = self.bus.get_irq();
 
         // Start of instruction execution
         if self.history_enabled {
@@ -660,8 +656,13 @@ where
         }
 
         // Check pending interrupts
-        if irq_exception != 0 {
-            let level = irq_exception;
+        let irq_exception_masked = match irq_exception {
+            Some(7) => 7,
+            Some(level) if level > self.regs.sr.int_prio_mask() => level,
+            _ => 0,
+        };
+        if irq_exception_masked != 0 {
+            let level = irq_exception_masked;
             if self
                 .breakpoints
                 .contains(&Breakpoint::InterruptLevel(level))