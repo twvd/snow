7e671ce70a866d0601b5be23d87eba4c53bd73b8
demik <demik475@gmail.com>
2026-05-12 11:58:39 +0200

    #310: first step of ICMP Proxy that handles ICMP Address Maks Requests\n Need further cleaning, debug props

--- nat/src/mactcp_helpers.rs
+++ nat/src/mactcp_helpers.rs
@@ -2,12 +2,14 @@
 //!
 //! This module enables helpers that are useful for MacTCP
 //! - ICMP delay
+//! - ICMP proxy-ish (handle Address Mask Requests)
 //! - RARP server
 
 use anyhow::Result;
+use smoltcp::phy::ChecksumCapabilities;
 use smoltcp::wire::{
     ArpHardware, ArpOperation, ArpPacket, EthernetAddress, EthernetFrame, EthernetProtocol,
-    IpProtocol, Ipv4Address, Ipv4Packet,
+    Icmpv4Message, Icmpv4Packet, IpProtocol, Ipv4Address, Ipv4Packet, Ipv4Repr,
 };
 use std::time::Duration;
 
@@ -62,6 +64,121 @@ pub fn handle_rarp_request(
     Ok(Some(buf))
 }
 
+/// Returns true if the packet is an ICMP Address Mask Request (type 17)
+pub fn needs_icmp_proxy(packet: &[u8]) -> bool {
+    let eth_hlen = EthernetFrame::<&[u8]>::header_len();
+    if packet.len() <= eth_hlen {
+        return false;
+    }
+    if EthernetFrame::new_unchecked(packet).ethertype() != EthernetProtocol::Ipv4 {
+        return false;
+    }
+    let ipv4 = match Ipv4Packet::new_checked(&packet[eth_hlen..]) {
+        Ok(p) => p,
+        Err(_) => return false,
+    };
+    if ipv4.next_header() != IpProtocol::Icmp {
+        return false;
+    }
+    Icmpv4Packet::new_checked(ipv4.payload())
+        .map(|icmp| icmp.msg_type() == Icmpv4Message::Unknown(17))
+        .unwrap_or(false)
+}
+
+/// Validate an ICMP Address Mask Request (type 17) and return the reply frame if ok
+pub fn handle_icmp_address_mask_request(
+    eth_frame: &EthernetFrame<&[u8]>,
+    gateway_ip: Ipv4Address,
+    gateway_mac: EthernetAddress,
+    subnet_prefix: u8,
+) -> Result<Option<Vec<u8>>> {
+    let ipv4 = match Ipv4Packet::new_checked(eth_frame.payload()) {
+        Ok(p) => p,
+        Err(_) => return Ok(None),
+    };
+    if ipv4.next_header() != IpProtocol::Icmp {
+        return Ok(None);
+    }
+    let icmp = match Icmpv4Packet::new_checked(ipv4.payload()) {
+        Ok(p) => p,
+        Err(_) => return Ok(None),
+    };
+    if icmp.msg_type() != Icmpv4Message::Unknown(17) || icmp.msg_code() != 0 {
+        return Ok(None);
+    }
+
+    let identifier = icmp.echo_ident();
+    let seq_no = icmp.echo_seq_no();
+
+    // It would be a better idea to be classful here, but the code in ethernet.rs is
+    // classless so let's follow that
+    let address_mask: u32 = if subnet_prefix == 0 {
+        0
+    } else {
+        !0u32 << (32 - subnet_prefix as u32)
+    };
+
+    let buf = build_icmp_address_mask_reply(
+        eth_frame.src_addr(),
+        gateway_ip,
+        gateway_mac,
+        address_mask,
+        identifier,
+        seq_no,
+    );
+    log::info!(
+        "ICMP: address mask request from {}, replying with {}",
+        ipv4.src_addr(),
+        Ipv4Address::from(address_mask.to_be_bytes())
+    );
+    Ok(Some(buf))
+}
+
+/// Build a full Ethernet + IPv4 + ICMP Address Mask Reply frame because smoltcp
+/// doesn't handle deprecated prehistoric stuff
+/// No idea if the answer should be unicast or broadcast, broadcast works for MacTCP
+fn build_icmp_address_mask_reply(
+    eth_dst: EthernetAddress,
+    gateway_ip: Ipv4Address,
+    gateway_mac: EthernetAddress,
+    address_mask: u32,
+    identifier: u16,
+    seq_no: u16,
+) -> Vec<u8> {
+    const ICMP_LEN: usize = 12;
+    let eth_hlen = EthernetFrame::<&[u8]>::header_len();
+    let ip_repr = Ipv4Repr {
+        src_addr: gateway_ip,
+        dst_addr: Ipv4Address::BROADCAST,
+        next_header: IpProtocol::Icmp,
+        payload_len: ICMP_LEN,
+        hop_limit: 1,
+    };
+    let total = eth_hlen + ip_repr.buffer_len() + ICMP_LEN;
+    let mut buf = vec![0u8; total];
+
+    let mut eth = EthernetFrame::new_unchecked(&mut buf);
+    eth.set_dst_addr(eth_dst);
+    eth.set_src_addr(gateway_mac);
+    eth.set_ethertype(EthernetProtocol::Ipv4);
+
+    let mut ipv4 = Ipv4Packet::new_unchecked(&mut buf[eth_hlen..]);
+    ip_repr.emit(&mut ipv4, &ChecksumCapabilities::default());
+
+    let icmp_start = eth_hlen + ip_repr.buffer_len();
+    let icmp_buf = &mut buf[icmp_start..];
+    icmp_buf[0] = 18;
+    icmp_buf[1] = 0;
+    icmp_buf[2] = 0;
+    icmp_buf[3] = 0;
+    icmp_buf[4..6].copy_from_slice(&identifier.to_be_bytes());
+    icmp_buf[6..8].copy_from_slice(&seq_no.to_be_bytes());
+    icmp_buf[8..12].copy_from_slice(&address_mask.to_be_bytes());
+    Icmpv4Packet::new_unchecked(icmp_buf).fill_checksum();
+
+    buf
+}
+
 /// Build a RARP reply frame with the specified IP.
 fn build_rarp_reply(
     client_mac: EthernetAddress,
@@ -73,22 +190,22 @@ fn build_rarp_reply(
         .to_be_bytes();
 
     let mut buf = vec![0u8; 42];
-    {
-        let mut frame = EthernetFrame::new_unchecked(&mut buf);
-        frame.set_dst_addr(client_mac);
-        frame.set_src_addr(gateway_mac);
-        frame.set_ethertype(EthernetProtocol::Unknown(0x8035));
 
-        let mut reply = ArpPacket::new_unchecked(frame.payload_mut());
-        reply.set_hardware_type(ArpHardware::Ethernet);
-        reply.set_protocol_type(EthernetProtocol::Ipv4);
-        reply.set_hardware_len(6);
-        reply.set_protocol_len(4);
-        reply.set_operation(ArpOperation::from(4u16));
-        reply.set_source_hardware_addr(gateway_mac.as_bytes());
-        reply.set_source_protocol_addr(&gateway_ip.octets());
-        reply.set_target_hardware_addr(client_mac.as_bytes());
-        reply.set_target_protocol_addr(&client_ip);
-    }
+    let mut frame = EthernetFrame::new_unchecked(&mut buf);
+    frame.set_dst_addr(client_mac);
+    frame.set_src_addr(gateway_mac);
+    frame.set_ethertype(EthernetProtocol::Unknown(0x8035));
+
+    let mut reply = ArpPacket::new_unchecked(frame.payload_mut());
+    reply.set_hardware_type(ArpHardware::Ethernet);
+    reply.set_protocol_type(EthernetProtocol::Ipv4);
+    reply.set_hardware_len(6);
+    reply.set_protocol_len(4);
+    reply.set_operation(ArpOperation::from(4u16));
+    reply.set_source_hardware_addr(gateway_mac.as_bytes());
+    reply.set_source_protocol_addr(&gateway_ip.octets());
+    reply.set_target_hardware_addr(client_mac.as_bytes());
+    reply.set_target_protocol_addr(&client_ip);
+
     buf
 }
